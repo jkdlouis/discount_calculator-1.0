32eeebb5d0038893a2814255413b7cf53df3844f
Louis Huang <0563louishuang@gmail.com>
2016-12-14 10:38:19 -0800

    button position fixed

--- js/auto_lead.js
+++ js/auto_lead.js
@@ -194,14 +194,14 @@
             frame += '<style>';
             frame += '.lead-frame-wrapper {' + style + 'color: #333; font-size: 14px; font-family: "Lato", Helvetica, Arial, sans-serif;}';
             frame += '.lead-frame-wrapper * { box-sizing: border-box; } ';
-            frame += '.lead-frame-container { overflow: hidden; width: 100%; background-color: #f6f6f6; height: 67rem; }';
+            frame += '.lead-frame-container { width: 100%; background-color: #f6f6f6;}';
             frame += '.lead-frame-wrapper .container-fluid { padding: 0; }';
-            frame += '.lead-frame-container .form-container { padding-top: 33px; padding-left: 54px;}';
-            frame += '.lead-frame-container .premium-container { text-align: center; background: url("images/green-background.png") no-repeat center; background-size: 100% 100%; padding: 150px 45px; }';
+            frame += '.lead-frame-container .form-container { padding-top: 33px; padding-left: 54px; position: relative;}';
+            frame += '.lead-frame-container .premium-container { text-align: center; background: url("images/green-background.png") no-repeat center; background-size: 100% 100%; padding-top: 50px; padding-bottom: 250px;}';
             frame += '.lead-frame-container .mobile-premium-container { width: 100%; height: 450px; padding: 10px; background: url("images/mobile-green-background.png") no-repeat center;  }';
             frame += '.lead-frame-container .premium-number { font-weight: bold; font-size: 50px; color: #ffffff; }';
             frame += '.lead-frame-container .premium-img { margin-bottom: 27px; margin-right: 19px;}';
-            frame += '.lead-frame-container .custom-title { font-family: "Lato"; font-size: 45px; font-weight: lighter; margin: 5px 0 35px; color: #252525; text-align: left; } ';
+            frame += '.lead-frame-container .custom-title { font-family: "Lato"; font-size: 50px; font-weight: lighter; margin: 5px 0 35px; color: #252525; text-align: left; } ';
             frame += '.lead-frame-container .title-slogan { font-size: 20px; font-weight: lighter; color: #878787; line-height: 1.5; margin-bottom: 35px; }';
             frame += '.lead-frame-container .info-title { font-size: 20px; font-weight: bold; color: #878787; line-height: 1.5; margin-bottom: 15px; margin-top: 0; }';
             frame += '.lead-frame-container .info-title-green {color: #37c7b4;}';
@@ -209,7 +209,7 @@
             frame += '.lead-frame-container select { font-size: 20px; font-family: open-sans; color: #b2b1b1; border: 1px solid #cccccc; margin: 9px 0; max-width: 548px; padding: 15px; width: 100%; border-radius: 6px; -webkit-appearance: none; -moz-appearance: none; appearance: none; cursor: pointer; height: 55px; background: white url("images/down-arrow.png") no-repeat scroll 100% center / 12% auto; }';
             frame += '.lead-frame-container .inc_premium {#b2b1b1; border: 1px solid #cccccc; margin: 9px 0; max-width: 548px; padding: 19px; width: 100%; border-radius: 8px; }';
             frame += '.lead-frame-container input[type=text] { width: 100%; max-width: 271px; margin-bottom: 8px; height: 55px; padding: 4px 6px; } ';
-            frame += '.lead-frame-container .lead-frame-btn { background-color: #168abe; border: 1px solid #2e6da4; border-radius: 4px; color: #ffffff; cursor: pointer; font-size: 18px; font-weight: bold; margin: 24px 0 24px 3%; padding: 10px 25px; width: 126px; height: 50px; float: left; text-decoration: none; position: relative; bottom: 95px; left: 1%; }';
+            frame += '.lead-frame-container .lead-frame-btn { background-color: #168abe; border: 1px solid #2e6da4; border-radius: 4px; color: #ffffff; cursor: pointer; font-size: 18px; font-weight: bold; padding: 10px 25px; width: 126px; height: 50px; float: left; text-decoration: none; position: absolute; bottom: 18px; left: 5.5%; }';
             frame += '.lead-frame-container .align-right { float: right; } .lead-frame-container .align-left { float: left; } ';
             frame += '.lead-frame-container .discount-services { padding-top: 15px; -moz-column-count:2; -webkit-column-count:2; column-count:2;}';
             frame += '.lead-frame-container label.regular-checkbox-label { display: inline; } .lead-frame-container .regular-checkbox { display: none; }';
@@ -217,7 +217,7 @@
             frame += '.lead-frame-container .regular-checkbox + label:active, .lead-frame-container .regular-checkbox:checked + label:active { box-shadow: 0 1px 3px rgba(0,0,0,0.05), inset 0px 1px 3px rgba(0,0,0,0.1); } ';
             frame += '.lead-frame-container .regular-checkbox:checked + label { background-color: #e9ecee; border: 1px solid #adb8c0; box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05), inset 15px 10px -12px rgba(255,255,255,0.1); color: #99a1a7; } ';
             frame += '.lead-frame-container .regular-checkbox:checked + label:after { content: "\u2714"; font-size: 14px; position: absolute; top: 0px; left: 2px; color: #99a1a7; } ';
-            frame += '@media screen and (min-device-width: 300px) and (max-device-width: 768px) { .lead-frame-wrapper { width: 100%; padding: 0; } .lead-frame-container { overflow: hidden; height: auto;} .lead-frame-container .form-container {padding: 5px; width: 100%; margin-bottom: 0; } .lead-frame-container .mobile-premium-container { width: 100%; height: 330px; background-size: 100% 100%; } .lead-frame-container .premium-img { margin: 105px 0 0 16%;} .lead-frame-container .premium-number {font-size: 20px;} .lead-frame-container .custom-title { font-size: 35px; } .lead-frame-container .title-slogan { font-size: 18px; } .lead-frame-container .custom-small-title { font-size: 15px; margin: 10 0; } .lead-frame-container select { height: 40px; padding: 0; font-size: 15px; padding: 5px 10px; } .lead-frame-container .lead-frame-btn { margin: 10px auto; float: left; position: relative; bottom: 330px; left: -3%; height: 40px; width: 100px; padding: 5px; } .lead-frame-container .info-title { font-size: 15px; margin-bottom: 0; } .lead-frame-container input[type=text] { height: 40px; } .lead-frame-container .discount-services {-moz-column-count:1; -webkit-column-count:1; column-count:1;} }';
+            frame += '@media screen and (min-device-width: 300px) and (max-device-width: 768px) { .lead-frame-wrapper { width: 100%; padding: 0; } .lead-frame-container { overflow: hidden; height: auto;} .lead-frame-container .form-container {padding: 5px 10px; width: 100%; margin-bottom: 0; position: relative; } .lead-frame-container .mobile-premium-container { width: 100%; height: 330px; background-size: 100% 100%; } .lead-frame-container .premium-img { margin: 105px 0 0 5%;} .lead-frame-container .premium-number {font-size: 20px;} .lead-frame-container .custom-title { font-size: 35px; } .lead-frame-container .title-slogan { font-size: 18px; } .lead-frame-container .custom-small-title { font-size: 15px; margin: 10 0; } .lead-frame-container select { height: 40px; padding: 0; font-size: 15px; padding: 5px 10px; } .lead-frame-container .lead-frame-btn { float: left; position: absolute; bottom: 270px; left: 3%; height: 40px; width: 100px; padding: 5px; } .lead-frame-container .info-title { font-size: 15px; margin-bottom: 0; } .lead-frame-container input[type=text] { height: 40px; } .lead-frame-container .discount-services {-moz-column-count:1; -webkit-column-count:1; column-count:1;} }';
             frame += '</style>';
             $(target).html(frame);
         },
@@ -279,7 +279,7 @@
             html += '<form class="form-inline form-container">';
             html += '<div class="form-group">';
             html += '<p class="custom-title">See how much you could be saving</p>';
-            html += '<h5 class="title-slogan">Compare your current auto insurance rate with other companies in your area and <br> see if you qualify for any discounts!</h5>';
+            html += '<h5 class="title-slogan">Compare your current auto insurance rate with other companies in your area and see if you qualify for any discounts!</h5>';
             html += '<h4 class="custom-small-title">What car do you drive?</h4>';
             car_year = this._select_field(years, 'car_year', 'car_year', 'car_year', 'Select Year');
             car_make = this._select_field(this.car_make, 'car_make', 'car_make', 'car_make', 'Select Make', 'cursor: not-allowed;');
@@ -553,13 +553,17 @@
                 6: {text: 'Air bag', weight: 4}
             },
             html = '<p class="custom-small-title"> Check discounts to see the possible savings </p>';
+            html += '<div class="row">';
+            html += '<div class="col-xs-12">';
             $.each(discounts, function (key, value) {
                 html += '<div class="checkbox" style="display: inline-flex; font-size: 20px; font-family: "Open Sans";">';
                 html += '<input type="checkbox" class="check-discount regular-checkbox" id="lead-discount-' + key + '" value="' + value.weight + '">';
                 html += '<label class="regular-checkbox-label" for="lead-discount-' + key + '"/>';
-                html += '<span style="font-weight: normal;"> ' + value.text + '</span></div><br/>';
+                html += '<span style="font-weight: normal;"> ' + value.text + '</span></div>';
             });
-            html += '<img src="images/3min.png" style="float:right; margin-top: -30px;">';
+            // html += '<img src="images/3min.png" style="float:right; margin-top: -30px;">';
+            html += '</div>';
+            html += '</div>';
             return html;
         },
 
